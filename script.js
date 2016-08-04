3d0d1aa49112cd3b0cb14849031989de091d3001
acmp1 <clarissamirandap@gmail.com>
2016-08-04 11:55:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,6 @@ $(document).ready(function(){
     $( "#accordion" ).accordion({
       heightStyle: "content"
     });
-    initMap();
     $("#bContacto").click(function(){
         $('#bContacto').addClass('active');
         $('#bInicio').removeClass('active');
